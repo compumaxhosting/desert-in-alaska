7cda74bdc02c8fc20aba52e1de0d0de8855d5d82
compumaxhosting <compumaxhost@gmail.com>
2026-04-24 21:54:00 +0530

    updated redirect links

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,18 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/commercial-hvac-services-detroit-mi.php",
-        destination:
-          "/services/commercial-hvac-detroit",
-        permanent: true,
+        destination: "/",
+        permanent: true, // ✅ 301 redirect (SEO safe)
       },
       {
         source: "/residential-hvac-services-detroit-michigan.php",
+        destination: "/services/commercial-hvac-detroit",
+        permanent: true, // ✅ 301 redirect (SEO safe)
+      },
+      {
+        source: "/shop.php",
         destination: "/",
-        permanent: true,
+        permanent: true, // ✅ 301 redirect (SEO safe)
       },
     ];
   },
